cc13a9f2474b7292cc2cb0def38348f9f378ea7d
Megan Horsfield <meganthadev@gmail.com>
2020-03-02 04:23:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,50 +1,50 @@
-class Artist
-  attr_accessor :name
-
-  @@all = []
-
+class Artist 
+attr_accessor :name, :songs
+@@all = []
   def initialize(name)
     @name = name
     @songs = []
+    @@all << self
   end
-
+  
+  def self.all
+    @@all
+  end
+  
   def add_song(song)
-    @songs << song 
+    @songs << song
+    song.artist = self
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
   end
   
   def self.find_or_create_by_name(name)
-     self.all.find { |artist| artist.name == name || self.create(name) }
-    end 
-       
-  
-  def self.find(name)
-    self.all.detect { |artist| artist.name == name }
-  end
-
-  
-  def self.add_song_by_name(song_name)
-    artist_name = Artist.new(name)
-    artist_name
-  end
-
-  def print_songs
-    @songs.each do |song|
-      puts song.name
-   end
- end
- 
- def self.all
-    @@all
-  end
-
-  def songs
-     @songs 
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
   end
   
-  def self.song_count
-    Song.all
-  end  
+   def self.find(name)
+    @@all.find do |artist|
+      artist.name == name
+    end
+  end
   
+  def self.create(name)
+    artist = self.new(name)
+    #@@all << artist
+    artist
+  end
   
-  
-end 
+  def save
+    @@all << self
+  end
+    
+  def print_songs()
+    puts @songs.collect {|x| x.name}
+  end
+end
