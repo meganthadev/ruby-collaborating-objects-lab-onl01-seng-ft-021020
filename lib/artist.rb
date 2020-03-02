62ca891c7c47e0ea0ab5bc3a33d334d70a9dc928
Megan Horsfield <meganthadev@gmail.com>
2020-03-02 04:21:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,7 +13,7 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
-     self.all.find { |artist| artist.name == name || self.new(name).save }
+     self.all.find { |artist| artist.name == name || self.create(name) }
     end 
        
   
